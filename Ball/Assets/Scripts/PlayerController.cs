e05cbf431fdca54f4749cbebed5c2f9ddab96fe6
Sepehr <main.sepehr@gmail.com>
2024-09-30 10:47:44 +0100

    code

--- Ball/Assets/Scripts/PlayerController.cs
+++ Ball/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rb;
+    public PlayerControls playerControls;
+    public Vector2 movement;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+        }
+        playerControls.Player.Move.performed += HandleMove;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Player.Move.performed -= HandleMove;
+        playerControls.Disable();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleLocomotion();
+    }
+
+    void HandleLocomotion()
+    {
+        rb.AddForce(movement);
+    }
+
+    void HandleMove(InputAction.CallbackContext context)
+    {
+        movement  = context.ReadValue<Vector2>();
+        Debug.Log(movement);
     }
 }
